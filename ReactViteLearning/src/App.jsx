62f610d047fe7ab17c0919ef069f039c05f7923c
Ryan Hoang <anhoang800140@gmail.com>
2025-01-11 20:30:07 +0700

    How to apply Ternary oeprator  for component

--- ReactViteLearning/src/App.jsx
+++ ReactViteLearning/src/App.jsx
@@ -1,9 +1,10 @@
-import Happy from "./components/Happy";
-import UnHappy from "./components/UnHappy";
-
+import Course from "./components/Course";
 function App() {
-  const isHappy = false;
-  return <div>{isHappy ? <Happy /> : <UnHappy />}</div>;
+  return (
+    <>
+      <Course />
+    </>
+  );
 }
 
 export default App;
