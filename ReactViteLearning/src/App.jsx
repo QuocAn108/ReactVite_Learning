0d99237665e0c5a618554db5530cdcbaa75002af
Ryan Hoang <anhoang800140@gmail.com>
2025-01-23 22:30:28 +0700

    Change color apply 2 way binding

--- ReactViteLearning/src/App.jsx
+++ ReactViteLearning/src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
 function App() {
-  const [username, setUsername] = useState();
-  const handleGetUsername = (e) => {
-    setUsername(e.target.value);
+  const colors = ["red", "green", "blue"];
+  const [color, setColor] = useState("red");
+  const handleSetColor = (index) => {
+    setColor(colors[index]);
   };
   return (
     <>
-      <div>Username: {username}</div>
-      <input type="text" onChange={(e) => handleGetUsername(e)} />
+      <div
+        style={{
+          background: color,
+          width: "70px",
+          height: "30px",
+          marginBottom: "10px",
+        }}
+      ></div>
+      <div>
+        <button onClick={() => handleSetColor(0)}>Red</button>
+        <button onClick={() => handleSetColor(1)}>Green</button>
+        <button onClick={() => handleSetColor(2)}>Blue</button>
+      </div>
     </>
   );
 }
