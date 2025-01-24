718fdb4fc138a0dcb5f57d375902f3ef89d9c1d8
RyanHoang <anhoang800140@gmail.com>
2025-01-24 16:53:16 +0700

    mount and unmount in ReactJS

--- ReactViteLearning/src/App.jsx
+++ ReactViteLearning/src/App.jsx
@@ -1,40 +1,14 @@
 import { useState } from "react";
+import Content from "./components/Content";
 function App() {
-  const [chooseProduct, serChooseProduct] = useState([]);
-  const products = [
-    { id: 1, name: "Product 1", price: 100 },
-    { id: 2, name: "Product 2", price: 200 },
-    { id: 3, name: "Product 3", price: 300 },
-  ];
-  const handleChooseProduct = (id) => {
-    if (!chooseProduct.includes(+id)) {
-      serChooseProduct((prev) => {
-        return [...prev, +id];
-      });
-    } else {
-      serChooseProduct((prev) => {
-        return prev.filter((item) => item !== +id);
-      });
-    }
+  const [show, setShow] = useState(false);
+  const handleShow = () => {
+    setShow(!show);
   };
-  console.log(chooseProduct);
   return (
     <>
-      <div>
-        {products.map((product, index) => {
-          return (
-            <div key={index}>
-              <label>{product.name}</label>
-              <input
-                type="checkbox"
-                value={product.id}
-                onChange={(e) => handleChooseProduct(e.target.value)}
-                checked={chooseProduct.includes(product.id)}
-              />
-            </div>
-          );
-        })}
-      </div>
+      <button onClick={handleShow}>Show</button>
+      {show && <Content />}
     </>
   );
 }
