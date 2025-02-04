d1ea4fa56cc29a0f7d06eb6410e6d16fcfc20cb9
Ryan Hoang <anhoang800140@gmail.com>
2025-02-04 18:16:19 +0700

    Using clean up function when using useEffect

--- ReactViteLearning/src/App.jsx
+++ ReactViteLearning/src/App.jsx
@@ -1,38 +1,11 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import Timer from "./components/Timer";
 function App() {
-  const [count, setCount] = useState(1);
-  const [post, setPosts] = useState([]);
-  const [color, setColor] = useState("");
-  // useEffect(() => {
-  //   fetch("https://jsonplaceholder.typicode.com/posts")
-  //     .then((response) => response.json())
-  //     .then((json) => setPosts(json));
-  // }, []);
-  // useEffect(() => {
-  //   console.log("useEffect");
-  // }, []);
-
-  useEffect(() => {
-    if (count % 2 == 0) {
-      setColor("red");
-    } else {
-      setColor("blue");
-    }
-  }, [count]);
+  const [show, setShow] = useState(false);
   return (
     <div>
-      <div>{count}</div>
-      <div
-        style={{
-          width: "100px",
-          height: "100px",
-          backgroundColor: color,
-        }}
-      ></div>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
-      {/* {post.map((post, index) => {
-        return <div key={index}>{post.title}</div>;
-      })} */}
+      <button onClick={() => setShow(!show)}>Show</button>
+      {show && <Timer />}
     </div>
   );
 }
