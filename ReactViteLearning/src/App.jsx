484c0f77dedd80a05323faaef580f9af834dec3e
Ryan Hoang <anhoang800140@gmail.com>
2025-02-04 17:46:52 +0700

    Method 1 useEffect noDependencies

--- ReactViteLearning/src/App.jsx
+++ ReactViteLearning/src/App.jsx
@@ -1,13 +1,19 @@
 import { useEffect, useState } from "react";
 function App() {
   const [count, setCount] = useState(1);
+  const [post, setPosts] = useState([]);
   useEffect(() => {
-    console.log("re-render");
+    // fetch("https://jsonplaceholder.typicode.com/posts")
+    //   .then((response) => response.json())
+    //   .then((json) => setPosts(json));
   });
   return (
     <div>
-      <div>{count}</div>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      {/* <div>{count}</div>
+      <button onClick={() => setCount(count + 1)}>Click me</button> */}
+      {post.map((post, index) => {
+        return <div key={index}>{post.title}</div>;
+      })}
     </div>
   );
 }
