12a569ea36cbbf9904d954a61b011959f973dde7
Ryan Hoang <anhoang800140@gmail.com>
2025-02-04 17:38:31 +0700

    using useEffect without dependencies

--- ReactViteLearning/src/App.jsx
+++ ReactViteLearning/src/App.jsx
@@ -1,63 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 function App() {
-  const [todo, setTodo] = useState({ id: "", name: "" });
-  const [todoList, setTodoList] = useState([
-    { id: 1, name: "Danh rang rua mat" },
-  ]);
-  const handleGetTodo = (value) => {
-    const randomId = Math.floor(Math.random(100000) * 100000);
-    const todo = {
-      id: randomId,
-      name: value,
-    };
-    setTodo(todo);
-  };
-  const handleSubmit = () => {
-    setTodoList([...todoList, todo]);
-    setTodo({
-      id: "",
-      name: "",
-    });
-  };
-  const handleDeleteTodo = (id) => {
-    const newTodoList = todoList.filter((todo) => todo.id !== id);
-    setTodoList(newTodoList);
-  };
+  const [count, setCount] = useState(1);
+  useEffect(() => {
+    console.log("re-render");
+  });
   return (
     <div>
-      <div>
-        <input
-          type="text"
-          onChange={(e) => handleGetTodo(e.target.value)}
-          value={todo.name}
-        />
-        <button onClick={handleSubmit}>Add</button>
-      </div>
-      <div>
-        <ul>
-          {todoList.map((todo, index) => {
-            return (
-              <div
-                style={{
-                  display: "flex",
-                  gap: "30px",
-                }}
-                key={index}
-              >
-                <li>{todo.name}</li>
-                <div
-                  style={{
-                    cursor: "pointer",
-                  }}
-                  onClick={() => handleDeleteTodo(todo.id)}
-                >
-                  X
-                </div>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
+      <div>{count}</div>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
     </div>
   );
 }
